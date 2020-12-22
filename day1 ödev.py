80468218f1db954615437795f8d9bb872833f240
Gizemvarol <76449413+Gizemvarol@users.noreply.github.com>
2020-12-22 18:21:52 +0300

    Add files via upload

---
+++ day1 ödev.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+x=input('ad soyad:')
+y=input('yaş:')
+z=input('boy:')
+t=input('kilo:')
+q=input('meslek:')
+print(x)
+print(y)
+print(z)
+print(t)
+print(q)
+
+
+# In[21]:
+
+
+type(x)
+
+
+# In[22]:
+
+
+type(y)
+
+
+# In[23]:
+
+
+type(z)
+
+
+# In[24]:
+
+
+type(t)
+
+
+# In[25]:
+
+
+type(q)
+
+
+# In[27]:
+
+
+print(x+' '+y+' '+z+' '+t+' '+q)
+
+
+# In[28]:
+
+
+data='%s %s %s %s %s' %(x,y,z,t,q)
+print(data)
+
+
+# In[43]:
+
+
+mylist=[x,int(y),int(z),int(t),q]
+print(mylist)
+
+
+# In[48]:
+
+
+type(mylist[1])
+
+
+# In[49]:
+
+
+type(mylist[0])
+
+
+# In[ ]:
+
+
+
+
